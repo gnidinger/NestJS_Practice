dbf4b8e71995a44f670aa7d650a80662aa5a1b73
gnidinger <gnidinger@gmail.com>
2024-02-05 12:03:50 +0900

    logging

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './global/filters/http-exception.filter';
+import { LoggingInterceptor } from './global/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
